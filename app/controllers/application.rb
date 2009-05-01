f9cd6725474e5110c65f420b19d5f63fd8a5a808
Sam <svn@localhost>
2009-05-01 15:52:33 +0000

    flash goes to helper

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -13,13 +13,4 @@ class ApplicationController < ActionController::Base
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
 
-  def flash_div *keys
-    keys.collect { |key| 
-      content_tag(:div, 
-        content_tag(:h1, key.to_s.capitalize) << flash[key],
-        :class => "flash flash_#{key}", 
-        :onClick => "new Effect.DropOut(this)") if flash[key] 
-    }.join
-  end
-
 end
