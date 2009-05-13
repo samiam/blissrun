7dc278a6446d696aed3ba74c4f7b09779d41de54
Sam <svn@localhost>
2009-05-13 23:54:12 +0000

    Polish up recaptcha code.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,3 @@
-require 'pp'
 # Test site w/o css turned on
 # Test site w/o javascript turned on
 # Test side w/images blocked
@@ -28,42 +27,13 @@ class HomeController < ActionController::Base
   end
 
   def contact
-    @subscribe = "&#x6d;&#x69;&#x6e;&#x64;&#x66;&#x75;&#x6c;&#x6e;&#x65;&#x73;&#x73;&#x2d;&#x73;&#x75;&#x62;&#x73;&#x63;&#x72;&#x69;&#x62;&#x65;&#x20;&#x40;&#x20;&#x62;&#x6c;&#x69;&#x73;&#x73;&#x72;&#x75;&#x6e;&#x2e;&#x6f;&#x72;&#x67;"
-    @unsubscribe = "&#x6d;&#x69;&#x6e;&#x64;&#x66;&#x75;&#x6c;&#x6e;&#x65;&#x73;&#x73;&#x2d;&#x75;&#x6e;&#x73;&#x75;&#x62;&#x73;&#x63;&#x72;&#x69;&#x62;&#x65;&#x20;&#x40;&#x20;&#x62;&#x6c;&#x69;&#x73;&#x73;&#x72;&#x75;&#x6e;&#x2e;&#x6f;&#x72;&#x67;"
-    # Spam methods
-    # Use temporary mailbox
-    # Use plussed addresses
-=begin
-    <script language="JavaScript">
-document.write('<a h'+'ref="m'+'ailt'+'o:'+'%66%6f%6f%40%62%61%72%2e%63%6f%6d">&#112;&#112;&#112;<\/a>');
-</script>
-=end
-    tmp = <<-EOF
-    <h3>Congratulations - you are human!</h3>
-     Here's a list of our email addresses:<p>
-     <a href='mailto:info@blissrun.org'>info@blissrun.org</a>
-     <p>
-     <a href='mailto:mindfulness-subscribe@blissrun.org'>mindfulness-subscribe@blissrun.org</a>
-     <p>
-     <a href='mailto:mindfulness-unsubscribe@blissrun.org'>mindfulness-unsubscribe@blissrun.org</a>
-    EOF
+  end
 
+  def addresses
     if request.xhr?
-      pp params
-      ans = verify_recaptcha
-      pp ans
-
-      if ans
-        render :update do |page|
-          page.hide 'recaptcha_form'
-          page.visual_effect :blind_down, 'recaptcha_blinddown'
-          page.replace_html 'recaptcha_blinddown', tmp
-        end
-      else
-        render :update do |page|
-          page.redirect_to 'contact'
-        end
-      end
+      @ans = verify_recaptcha
+    else
+      redirect_to root_url
     end
   end
 
