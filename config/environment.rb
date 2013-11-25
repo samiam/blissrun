d6e6a918a29fdc1c7a652891e9f394a2e436cdb9
Sam <sam+git@blissrun.org>
2013-11-24 20:23:19 -0500

    Add UTF-8 to config to render docs properly

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,10 @@
 # you don't control web/app server and can't set it the proper way
 ENV['RAILS_ENV'] = 'production'
 
+# To render docs properly - bluehost uses ASCII by default
+Encoding.default_internal = 'UTF-8'
+Encoding.default_external = 'UTF-8'
+
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.18' unless defined? RAILS_GEM_VERSION
 
